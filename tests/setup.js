1389316ec39fc1621f35e8457561f6d97f753df7
HexaField <joshfield999@gmail.com>
2023-03-03 14:33:01 +1100

    rename to etherealengine

--- tests/setup.js
+++ tests/setup.js
@@ -6,24 +6,24 @@ require("ts-node").register({
 })
 
 process.on('SIGTERM', async (err) => {
-  console.log('[XREngine Tests]: Server SIGTERM')
+  console.log('[Engine Tests]: Server SIGTERM')
   console.log(err)
 })
 process.on('SIGINT', () => {
-  console.log('[XREngine Tests]: RECEIVED SIGINT')
+  console.log('[Engine Tests]: RECEIVED SIGINT')
   process.exit()
 })
 
 //emitted when an uncaught JavaScript exception bubbles
 process.on('uncaughtException', (err) => {
-  console.log('[XREngine Tests]: UNCAUGHT EXCEPTION')
+  console.log('[Engine Tests]: UNCAUGHT EXCEPTION')
   console.log(err)
   process.exit()
 })
 
 //emitted whenever a Promise is rejected and no error handler is attached to it
 process.on('unhandledRejection', (reason, p) => {
-  console.log('[XREngine Tests]: UNHANDLED REJECTION')
+  console.log('[Engine Tests]: UNHANDLED REJECTION')
   console.log(reason)
   console.log(p)
   process.exit()

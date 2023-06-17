cac2c481a257b3d4cd347316f5bcd1fa5f1a476d
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2023-06-17 12:44:37 +0530

    feat: defineComponent

--- engine/MapComponent.ts
+++ engine/MapComponent.ts
@@ -1,5 +1,10 @@
-import { createMappedComponent } from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
+import {useEffect} from 'react'
 import { Vector3 } from 'three'
+
+import { createMappedComponent, defineComponent } from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
+import { SCENE_COMPONENT_MAP, _updateMap, serializeMap } from './MapFunctions'
+import { isClient } from '@etherealengine/engine/src/common/functions/getEnvironment'
+import { useEntityContext } from '@etherealengine/engine/src/ecs/functions/EntityFunctions'
 
 export type MapComponentType = {
   apiKey: string
--- engine/MapFunctions.ts
+++ engine/MapFunctions.ts
@@ -1,36 +1,43 @@
-import { ComponentJson } from '@etherealengine/common/src/interfaces/SceneInterface'
-import { defaultAvatarHalfHeight } from '@etherealengine/engine/src/avatar/functions/spawnAvatarReceptor'
-// import { DebugNavMeshComponent } from '@etherealengine/engine/src/debug/DebugNavMeshComponent'
-import { Engine } from '@etherealengine/engine/src/ecs/classes/Engine'
-import { EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
-import { Entity } from '@etherealengine/engine/src/ecs/classes/Entity'
-import { addComponent, defineQuery, getAllComponents, getComponent, hasComponent, setComponent } from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
-import { getState } from '@etherealengine/hyperflux'
-
 import { debounce } from 'lodash'
 import { Group, Mesh, Object3D } from 'three'
 import { Text } from 'troika-three-text'
 
+import { ComponentJson } from '@etherealengine/common/src/interfaces/SceneInterface'
+import { defaultAvatarHalfHeight } from '@etherealengine/engine/src/avatar/functions/spawnAvatarReceptor'
 import { isClient } from '@etherealengine/engine/src/common/functions/getEnvironment'
-import { MapComponent, MapComponentType } from './MapComponent'
-import { MapAction, mapReducer } from './MapReceptor'
-import { getPhases, startPhases } from './functions/PhaseFunctions'
-import { getStartCoords } from './getStartCoords'
-import { addChildFast, setPosition } from './util'
-import { NavMeshComponent } from './helpers/NavMeshComponent'
+// import { DebugNavMeshComponent } from '@etherealengine/engine/src/debug/DebugNavMeshComponent'
+import { Engine } from '@etherealengine/engine/src/ecs/classes/Engine'
+import { EngineState } from '@etherealengine/engine/src/ecs/classes/EngineState'
+import { Entity } from '@etherealengine/engine/src/ecs/classes/Entity'
+import {
+  addComponent,
+  defineQuery,
+  getAllComponents,
+  getComponent,
+  hasComponent,
+  setComponent
+} from '@etherealengine/engine/src/ecs/functions/ComponentFunctions'
 import { GroupComponent, Object3DWithEntity } from '@etherealengine/engine/src/scene/components/GroupComponent'
 import { SceneAssetPendingTagComponent } from '@etherealengine/engine/src/scene/components/SceneAssetPendingTagComponent'
+import { getState } from '@etherealengine/hyperflux'
+
+import { getPhases, startPhases } from './functions/PhaseFunctions'
+import { getStartCoords } from './getStartCoords'
+import { NavMeshComponent } from './helpers/NavMeshComponent'
+import { MapComponent, MapComponentType } from './MapComponent'
+import { MapAction, mapReducer } from './MapReceptor'
+import { addChildFast, setPosition } from './util'
 
 export const SCENE_COMPONENT_MAP = 'map'
 export const SCENE_COMPONENT_MAP_DEFAULT_VALUES = {}
 
-export const deserializeMap = (entity: Entity, json: ComponentJson<MapComponentType>) => {
+export const deserializeMap = (entity: Entity, json: ComponentJson<typeof MapComponent>) => {
   const sceneAssetPendingTagQuery = defineQuery([SceneAssetPendingTagComponent])
   if (isClient) {
-    if(sceneAssetPendingTagQuery.length > 0) {
+    if (sceneAssetPendingTagQuery.length > 0) {
       createMap(entity, json.props as MapComponentType)
     }
-    
+
     // if (getState(EngineState).isEditor) {
     //   const components = getAllComponents(entity)
     //   components.push(SCENE_COMPONENT_MAP)
@@ -39,7 +46,7 @@ export const deserializeMap = (entity: Entity, json: ComponentJson<MapComponentT
 }
 
 export const createMap = async (entity: Entity, args: MapComponentType) => {
-  if(getState(EngineState).isEditor && hasComponent(entity, MapComponent)) {
+  if (getState(EngineState).isEditor && hasComponent(entity, MapComponent)) {
     _updateMap(entity, args)
     return
   }
@@ -99,13 +106,15 @@ export const createMap = async (entity: Entity, args: MapComponentType) => {
 }
 
 export const _updateMap = async (entity: Entity, props: any) => {
-
   // only update on some property changes
-  if(!(
-    Object.keys(props).includes('startLatitude')
-    || Object.keys(props).includes('startLongitude')
-    || Object.keys(props).includes('useDeviceGeolocation'))
-  ) return
+  if (
+    !(
+      Object.keys(props).includes('startLatitude') ||
+      Object.keys(props).includes('startLongitude') ||
+      Object.keys(props).includes('useDeviceGeolocation')
+    )
+  )
+    return
 
   const args = getComponent(entity, MapComponent)
   const center = await getStartCoords(args)
@@ -131,23 +140,23 @@ export const _updateMap = async (entity: Entity, props: any) => {
   }
   subScene.children = subSceneChildren
 }
-export const updateMap = debounce((entity, args) => _updateMap(entity, args), 500) as any as (entity: Entity, props: any) => void
+export const updateMap = debounce((entity, args) => _updateMap(entity, args), 500) as any as (
+  entity: Entity,
+  props: any
+) => void
 
 export const serializeMap = (entity: Entity) => {
   const mapComponent = getComponent(entity, MapComponent)
   return {
-    name: SCENE_COMPONENT_MAP,
-    props: {
-      apiKey: mapComponent.apiKey,
-      name: mapComponent.name,
-      scale: mapComponent.scale,
-      useTimeOfDay: mapComponent.useTimeOfDay,
-      useDirectionalShadows: mapComponent.useDirectionalShadows,
-      useDeviceGeolocation: mapComponent.useDeviceGeolocation,
-      startLatitude: mapComponent.startLatitude,
-      startLongitude: mapComponent.startLongitude,
-      showRasterTiles: mapComponent.showRasterTiles,
-      enableDebug: mapComponent.enableDebug
-    }
+    apiKey: mapComponent.apiKey,
+    name: mapComponent.name,
+    scale: mapComponent.scale,
+    useTimeOfDay: mapComponent.useTimeOfDay,
+    useDirectionalShadows: mapComponent.useDirectionalShadows,
+    useDeviceGeolocation: mapComponent.useDeviceGeolocation,
+    startLatitude: mapComponent.startLatitude,
+    startLongitude: mapComponent.startLongitude,
+    showRasterTiles: mapComponent.showRasterTiles,
+    enableDebug: mapComponent.enableDebug
   }
 }
